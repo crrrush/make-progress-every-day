1c8c3cd8ef22528c3210c47465287ba26639ea50
crrrush <2293656642@qq.com>
2022-12-10 23:11:04 +0800

    valid-parentheses

---
+++ day 48/day 48/test.cpp
@@ -0,0 +1,174 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+//
+//20. 有效的括号
+//https ://leetcode.cn/problems/valid-parentheses/description/
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+//
+//有效字符串需满足：
+//
+//左括号必须用相同类型的右括号闭合。
+//左括号必须以正确的顺序闭合。
+//每个右括号都有一个对应的相同类型的左括号。
+//
+//
+//
+//示例 1：
+//
+//输入：s = "()"
+//输出：true
+//
+//示例 2：
+//
+//输入：s = "()[]{}"
+//输出：true
+//
+//示例 3：
+//
+//输入：s = "(]"
+//输出：false
+//
+//
+//
+//提示：
+//
+//1 <= s.length <= 104
+//s 仅由括号 '()[]{}' 组成
+//
+
+
+typedef char STDataType;
+typedef struct Stack
+{
+	STDataType* _a;
+	int _top;		// 栈顶
+	int _capacity;  // 容量 
+}Stack;
+
+// 初始化栈 
+void StackInit(Stack* ps);
+
+// 入栈 
+void StackPush(Stack* ps, STDataType data);
+
+// 出栈 
+void StackPop(Stack* ps);
+
+// 获取栈顶元素 
+STDataType StackTop(Stack* ps);
+
+// 获取栈中有效元素个数 
+int StackSize(Stack* ps);
+
+// 检测栈是否为空，如果为空返回非零结果，如果不为空返回0 
+int StackEmpty(Stack* ps);
+
+// 销毁栈 
+void StackDestroy(Stack* ps);
+
+
+// 初始化栈 
+void StackInit(Stack* ps)
+{
+	assert(ps);
+	ps->_a = NULL;
+	ps->_capacity = ps->_top = 0;
+}
+
+// 入栈 
+void StackPush(Stack* ps, STDataType data)
+{
+	assert(ps);
+	if (ps->_top == ps->_capacity)
+	{
+		int newcapacity = ps->_capacity == 0 ? 4 : ps->_capacity * 2;
+		STDataType* tmp = (STDataType*)realloc(ps->_a, newcapacity * sizeof(STDataType));
+		if (tmp == NULL)
+		{
+			perror("realloc failed");
+			exit(-1);
+		}
+		ps->_a = tmp;
+		ps->_capacity = newcapacity;
+	}
+	ps->_a[ps->_top] = data;
+	ps->_top++;
+}
+
+// 出栈 
+void StackPop(Stack* ps)
+{
+	assert(ps);
+	assert(!StackEmpty(ps));
+	ps->_top--;
+}
+
+// 获取栈顶元素 
+STDataType StackTop(Stack* ps)
+{
+	assert(ps);
+	assert(!StackEmpty(ps));
+	return ps->_a[ps->_top - 1];
+}
+
+// 获取栈中有效元素个数 
+int StackSize(Stack* ps)
+{
+	assert(ps);
+	return ps->_top;
+}
+
+// 检测栈是否为空，如果为空返回非零结果，如果不为空返回0 
+int StackEmpty(Stack* ps)
+{
+	assert(ps);
+
+	return ps->_top == 0;
+}
+
+// 销毁栈 
+void StackDestroy(Stack* ps)
+{
+	assert(ps);
+	free(ps->_a);
+	ps->_capacity = ps->_top = 0;
+}
+
+
+
+bool isValid(char* s) {
+	Stack c;
+	StackInit(&c);
+	while (*s)
+	{
+		if (*s == '(' || *s == '{' || *s == '[')
+		{
+			StackPush(&c, *s);
+		}
+		else
+		{
+			if (StackEmpty(&c))
+			{
+				StackDestroy(&c);
+				return false;
+			}
+			char tmp = StackTop(&c);
+			StackPop(&c);
+			if ((tmp == '(' && *s != ')') ||
+				(tmp == '{' && *s != '}') ||
+				(tmp == '[' && *s != ']'))
+			{
+				StackDestroy(&c);
+				return false;
+			}
+		}
+		++s;
+	}
+	int flag = 1;
+	if (!StackEmpty(&c))
+	{
+		flag = 0;
+	}
+	StackDestroy(&c);
+	return flag;
+}
