3201fe62a039e023b86853787adda2a4990e4a5c
crrrush <2293656642@qq.com>
2022-12-20 21:51:05 +0800

    reverse-string

---
+++ day 53/day 53/test.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//
+//344. 反转字符串
+//https ://leetcode.cn/problems/reverse-string/
+//编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+//不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+//
+//
+//
+//示例 1：
+//
+//输入：s = ["h", "e", "l", "l", "o"]
+//输出：["o", "l", "l", "e", "h"]
+//
+//示例 2：
+//
+//输入：s = ["H", "a", "n", "n", "a", "h"]
+//输出：["h", "a", "n", "n", "a", "H"]
+//
+//
+//
+//提示：
+//
+//1 <= s.length <= 105
+//s[i] 都是 ASCII 码表中的可打印字符
+//
+
+
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        //reverse(s.begin(), s.end());
+        auto begin = s.begin();
+        auto end = s.end() - 1;
+        while (begin < end)
+        {
+            char tmp = *begin;
+            *begin = *end;
+            *end = tmp;
+            ++begin;
+            --end;
+        }
+    }
+};
+
