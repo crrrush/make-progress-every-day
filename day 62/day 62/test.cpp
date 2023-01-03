dd6e1a80aa3f115e03d019cf142aecb43a6b927a
crrrush <2293656642@qq.com>
2023-01-03 23:36:16 +0800

    a easy question in my freshmen time

---
+++ day 62/day 62/test.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//我是大V
+//链接：https://www.nowcoder.com/questionTerminal/5c329570ba034871a96299df21e80e51
+//来源：牛客网
+//
+//每个人都想成为大V(VIP：Very Important Person)，但要一点一点积累才行，先从小v做起。要求输出由小写字母v组成的大V。
+//
+//输入描述 :
+//
+//无
+//
+//
+//
+//输出描述:
+//
+//v   v
+//
+//v v
+//
+//v
+//
+//
+//备注 :
+//
+//换行使用转义字符‘\n’
+//
+
+
+#include<stdio.h>
+
+int main()
+{
+    printf("v   v\n v v\n  v");
+
+    return 0;
+}
