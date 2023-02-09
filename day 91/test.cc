17386e5543d961129e4c1b37ce32ab4585569b6e
crrrush <2293656642@qq.com>
2023-02-09 20:04:18 +0800

    learns to add

---
+++ day 91/test.cc
@@ -0,0 +1,40 @@
+
+// 牛牛学加法
+// 链接：https://www.nowcoder.com/questionTerminal/07fde707001048db8342a4be16c7d4b0
+// 来源：牛客网
+
+// 给你两个整数，要求输出这两个整数的和
+
+
+// 输入描述:
+
+// 输入两个整数 a, b （0 <= a, b <= 1000）
+
+
+
+// 输出描述:
+
+// 输出一个整数.
+
+// 示例1
+// 输入
+
+// 1 2
+
+// 输出
+
+// 3
+
+#include<stdio.h>
+
+
+int main()
+{
+    int a = 0,b = 0;
+    
+    scanf("%d%d",&a,&b);
+    
+    printf("%d\n",a + b);
+    
+    return 0;
+}
