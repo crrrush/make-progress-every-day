8d9506af0090f17cdacdbf90d604955e9967e873
crrrush <90398252+crrrush@users.noreply.github.com>
2023-04-30 12:35:51 +0800

    search-insert-position

---
+++ day 168/test.cc
@@ -0,0 +1,62 @@
+
+// 35. 搜索插入位置
+
+
+// https://leetcode.cn/problems/search-insert-position/
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+// 请必须使用时间复杂度为 O(log n) 的算法。
+
+ 
+
+// 示例 1:
+
+// 输入: nums = [1,3,5,6], target = 5
+// 输出: 2
+
+// 示例 2:
+
+// 输入: nums = [1,3,5,6], target = 2
+// 输出: 1
+
+// 示例 3:
+
+// 输入: nums = [1,3,5,6], target = 7
+// 输出: 4
+
+ 
+
+// 提示:
+
+//     1 <= nums.length <= 104
+//     -104 <= nums[i] <= 104
+//     nums 为 无重复元素 的 升序 排列数组
+//     -104 <= target <= 104
+
+
+
+
+class Solution {
+public:
+    //O(log n) 二分查找
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0,right = nums.size() - 1;
+        while(left < right - 1)
+        {
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] > target)
+                right = mid;
+            else if(nums[mid] < target)
+                left = mid;
+            else
+                return mid;
+        }
+
+        if(nums[left] >= target)
+            return left;
+        else if(nums[right] < target)
+            return right + 1;
+        else
+            return right;
+    }
+};
