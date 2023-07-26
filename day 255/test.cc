b8439c9c54a6f0bbf759e605439dd48a41621c99
crrrush <90398252+crrrush@users.noreply.github.com>
2023-07-26 13:41:31 +0800

    search-in-a-binary-search-tree

---
+++ day 255/test.cc
@@ -0,0 +1,54 @@
+
+
+// 700. 二叉搜索树中的搜索
+// https://leetcode.cn/problems/search-in-a-binary-search-tree/
+// 给定二叉搜索树（BST）的根节点 root 和一个整数值 val。
+
+// 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 null 。
+
+ 
+
+// 示例 1:
+
+
+// 输入：root = [4,2,7,1,3], val = 2
+// 输出：[2,1,3]
+// 示例 2:
+
+
+// 输入：root = [4,2,7,1,3], val = 5
+// 输出：[]
+ 
+
+// 提示：
+
+// 数中节点数在 [1, 5000] 范围内
+// 1 <= Node.val <= 107
+// root 是二叉搜索树
+// 1 <= val <= 107
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == nullptr) return nullptr;
+
+        if(root->val == val) 
+            return root;
+        else if(root->val > val) 
+            return searchBST(root->left, val);
+        else 
+            return searchBST(root->right, val);
+    }
+};
