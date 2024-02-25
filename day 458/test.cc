cc4e13f629beb5f82e33a4a4b7315fbfe1f55633
crrrush <90398252+crrrush@users.noreply.github.com>
2024-02-25 22:43:55 +0800

    maximum-binary-tree

---
+++ day 458/test.cc
@@ -0,0 +1,83 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2024-02-25 22:42:46
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2024-02-25 22:43:21
+ * @FilePath: \every-little-progress\day 458\test.cc
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
+// 654. 最大二叉树
+// https://leetcode.cn/problems/maximum-binary-tree/description/
+// 给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
+
+// 创建一个根节点，其值为 nums 中的最大值。
+// 递归地在最大值 左边 的 子数组前缀上 构建左子树。
+// 递归地在最大值 右边 的 子数组后缀上 构建右子树。
+// 返回 nums 构建的 最大二叉树 。
+
+ 
+
+// 示例 1：
+
+
+// 输入：nums = [3,2,1,6,0,5]
+// 输出：[6,3,5,null,2,0,null,null,1]
+// 解释：递归调用如下所示：
+// - [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+//     - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+//         - 空数组，无子节点。
+//         - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+//             - 空数组，无子节点。
+//             - 只有一个元素，所以子节点是一个值为 1 的节点。
+//     - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+//         - 只有一个元素，所以子节点是一个值为 0 的节点。
+//         - 空数组，无子节点。
+// 示例 2：
+
+
+// 输入：nums = [3,2,1]
+// 输出：[3,null,2,null,1]
+ 
+
+// 提示：
+
+// 1 <= nums.length <= 1000
+// 0 <= nums[i] <= 1000
+// nums 中的所有整数 互不相同
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* to_tree(vector<int>::iterator begin, vector<int>::iterator end)
+    {
+        if(begin == end) return nullptr;
+        auto it = begin, max = begin;
+        while(it != end)
+        {
+            if(*it > *max) max = it;
+            ++it;
+        }
+
+        TreeNode* left = to_tree(begin, max);
+        TreeNode* right = to_tree(max + 1, end);
+        
+        TreeNode* root = new TreeNode(*max, left, right);
+        return root;
+    }
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return to_tree(nums.begin(), nums.end());
+    }
+};
