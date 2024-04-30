318de5c27b87213cebab1674be1b778f9aeb0387
crrrush <90398252+crrrush@users.noreply.github.com>
2024-04-30 20:08:33 +0800

    counting-bits

---
+++ day 519/test.cc
@@ -0,0 +1,69 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2024-04-30 20:07:20
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2024-04-30 20:08:09
+ * @FilePath: \every-little-progress\day 519\test.cc
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
+// 338. 比特位计数
+// https://leetcode.cn/problems/counting-bits/description/
+// 给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans 作为答案。
+
+ 
+
+// 示例 1：
+
+// 输入：n = 2
+// 输出：[0,1,1]
+// 解释：
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 示例 2：
+
+// 输入：n = 5
+// 输出：[0,1,1,2,1,2]
+// 解释：
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+ 
+
+// 提示：
+
+// 0 <= n <= 105
+ 
+
+// 进阶：
+
+// 很容易就能实现时间复杂度为 O(n log n) 的解决方案，你可以在线性时间复杂度 O(n) 内用一趟扫描解决此问题吗？
+// 你能不使用任何内置函数解决此问题吗？（如，C++ 中的 __builtin_popcount ）
+
+
+class Solution {
+    int count_one(int n)
+    {
+        int cnt = 0;
+        while(n)
+        {
+            n &= (n - 1);
+            ++cnt;
+        }
+        return cnt;
+    }
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1);
+        int set = 1;
+        int cnt = 0;
+        for(int i = 0;i <= n;++i) ans[i] = count_one(i);
+
+        return ans;
+    }
+};
